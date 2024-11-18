14cdb580f2156a0a7446566db73b59d87ad3a1ec
Prabhu R <prabhurprabhur063@gmail.com>
2024-11-18 23:41:10 +0530

    shop

---
+++ ex.js
@@ -0,0 +1,6 @@
+var inp=document.getElementById('in')
+
+function fu(){
+    var inputvalue=inp.value;
+    document.getElementById('disp').innerHTML=inputvalue;
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+function vanish()
+{
+    document.getElementById('span_tag').style.display='none';
+}
+function success(){
+    alert("Login Successful");
+
+ 
+}
+
